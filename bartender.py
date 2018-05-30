add214dcbe84ecc9990ee256a3afd7b4a44c5b38
brycedewitt <dewitt.b@husky.neu.edu>
2018-05-30 13:26:42 -0400

    Update bartender.py

--- bartender.py
+++ bartender.py
@@ -155,7 +155,7 @@ class Bartender(MenuDelegate):
         presentIng = 0
         for ing in ingredients.keys():
           for p in self.pump_configuration.keys():
-            if (ing == self.pump_configuration[p]["value"]):
+            if (ing == self.pump_configuration[p]["value"] and self.pump_configuration[p]["loaded?"] == true):
               presentIng += 1
         if (presentIng == len(ingredients.keys())):
           i.visible = True
