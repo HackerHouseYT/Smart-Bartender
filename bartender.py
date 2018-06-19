469104bcb923a366edef20bf5df88c8baf04f248
brycedewitt <dewitt.b@husky.neu.edu>
2018-06-19 11:22:24 -0400

    Update bartender.py

--- bartender.py
+++ bartender.py
@@ -269,7 +269,6 @@ class Bartender(MenuDelegate):
     # cancel any button presses while the drink is being made
     # self.stopInterrupts()
     self.running = True
-    self.startInterrupts()
 
     # Parse the drink ingredients and spawn threads for pumps
     maxTime = 0
