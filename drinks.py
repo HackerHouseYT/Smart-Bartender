2dfdaa4c8c8719cc1d49945bb621a8744990c0be
brycedewitt <dewitt.b@husky.neu.edu>
2018-06-21 23:30:03 -0400

    Update drinks.py

--- drinks.py
+++ drinks.py
@@ -63,7 +63,7 @@ drink_options = [
 	{"name": "Vodka", "value": "vodka"},
 	{"name": "Coke", "value": "coke"},
 	{"name": "Tequila", "value": "tequila"},
-	{"name": "Rum", "value": "rum,"},
+	{"name": "Rum", "value": "rum"},
 	{"name": "Tonic Water", "value": "tonicwater"},
 	{"name": "Red Bull", "value": "redbull"},
 	{"name": "Jager", "value": "jager"},
