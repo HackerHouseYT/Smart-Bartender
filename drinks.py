1c7384c90243a79c73028ee69c519764c3d4d737
brycedewitt <dewitt.b@husky.neu.edu>
2018-07-20 20:49:00 -0400

    Update drinks.py

--- drinks.py
+++ drinks.py
@@ -1,73 +1,11 @@
 # drinks.py
 drink_list = [
 	{
-		"name": "CaptPhillips",
+		"name": "Abbie",
 		"ingredients": {
-			"rum": 66,
+			"rum": 25,
 			"coke": 200
 		},
-	}, {
-		"name": "Run All",
-		"ingredients": {
-			"gin": 200,
-			"tequila": 200,
-			"vodka": 200,
-			"redbull": 200,
-			"coke": 200,
-			"tonicwater": 200,
-			"jager": 200,
-			"rum": 200
-		}
-	}, {
-		"name": "Rush Beta",
-		"ingredients": {
-			"gin": 20,
-			"tequila": 20,
-			"vodka": 20,
-			"redbull": 55,
-			"coke": 100,
-			"tonicwater": 180
-		}
-	}, {
-		"name": "MyNewHaircut",
-		"ingredients": {
-			"jager": 44,
-			"redbull": 245
-		}
-	}, {
-		"name": "Camp Juul",
-		"ingredients": {
-			"gin": 88,
-			"tonicwater": 132
-		}
-	}, {
-		"name": "SratPack",
-		"ingredients": {
-			"vodka": 60,
-			"tonicwater": 260
-		}
-	}, {
-		"name": "LaVidaLoca",
-		"ingredients": {
-			"tequila": 45,
-			"jager": 5,
-			"tonicwater": 5
-		}
-	}, {
-		"name": "ShotTequila",
-		"ingredients": {
-			"tequila": 44
-		}
-	}, {
-		"name": "HighSchool",
-		"ingredients": {
-			"vodka": 44
-		}
-	}, {
-		"name" : "Jager",
-		"ingredients": {
-			"jager": 44
-		}
 	}
 ]
 
